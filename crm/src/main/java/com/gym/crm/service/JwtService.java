7fac159e7d9c7a5753e6329b763e783b83648ec5
AbdullohMaraimov <maraimovabdulloh.laptop2022@gmail.com>
2024-10-02 15:24:02 +0500

    spring boot task done

---
+++ crm/src/main/java/com/gym/crm/service/JwtService.java
@@ -0,0 +1,12 @@
+package com.gym.crm.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface JwtService {
+
+    String generateToken(UserDetails userDetails);
+
+    String extractUsername(String token);
+
+    boolean isTokenValid(String token, UserDetails userDetails);
+}
---
+++ crm/src/main/java/com/gym/crm/service/TrainerService.java
@@ -0,0 +1,24 @@
+package com.gym.crm.service;
+
+import com.gym.crm.model.dto.request.TrainerRequest;
+import com.gym.crm.model.dto.response.RegistrationResponse;
+import com.gym.crm.model.dto.response.TrainerResponse;
+
+import java.util.List;
+
+public interface TrainerService {
+
+    RegistrationResponse create(TrainerRequest trainer);
+
+    TrainerResponse update(String username, TrainerRequest trainer);
+
+    TrainerResponse findByUsername(String username);
+
+    List<TrainerResponse> findAll();
+
+    void updatePassword(String username, String oldPassword, String newPassword);
+
+    void deActivateUser(String username);
+
+    void activateUser(String username);
+}
